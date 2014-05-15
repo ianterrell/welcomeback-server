a0d6f77c49835d3e8c991a97865d65778c500e32
Ian Terrell <ian.terrell@gmail.com>
2014-05-14 22:38:39 -0700

    save pretty json

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,14 @@ class Event < ActiveRecord::Base
   validates_uniqueness_of :name
   
   validate :data_is_json, unless: ->{ self.data.nil? }
+  after_validation :prettyup, if: -> { data.is_json? }
   
   def data_is_json
     errors[:data] << "not in json format" unless data.is_json?
+  end
+  
+  def prettyup
+    self.data = JSON.pretty_generate(JSON.parse(self.data))
   end
   
   def as_json(options={})
